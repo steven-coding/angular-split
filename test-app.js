f3050e353c48b406ccdc440b6d6181581a831825
Steven <steven_thielemann@yahoo.de>
2018-05-21 20:35:48 +0200

    cleanup js-files

--- test-app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("core-js/es7/reflect");
-require("zone.js/dist/zone");
-var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-var test_app_module_1 = require("./test-app/test-app.module");
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(test_app_module_1.TestAppModule);
-//# sourceMappingURL=test-app.js.map
